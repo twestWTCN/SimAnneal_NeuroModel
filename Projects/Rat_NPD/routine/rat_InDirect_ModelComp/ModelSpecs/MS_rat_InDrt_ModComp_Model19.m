116ed09f5c1fed9e06f9b8d42d2ebbc169dbe7f2
Tim West <twestWTCN@users.noreply.github.com>
2019-01-30 12:26:23 +0000

    AdjustedModelSpace

---
+++ Projects/Rat_NPD/routine/rat_InDirect_ModelComp/ModelSpecs/MS_rat_InDrt_ModComp_Model19.m
@@ -0,0 +1,99 @@
+function [R p m uc] = MS_rat_InDrtModComp_Model19(R)
+%% Prepare Model
+m.m = 6; % # of sources
+m.x = {[0 0 0 0 0 0 0 0]  [0 0]  [0 0]  [0 0]  [0 0]  [0 0]}; % Initial states
+m.Gint = [14 1 1 1 1 1];
+m.Tint = [4 1 1 1 1 1];
+m.n =  size([m.x{:}],2); % Number of states
+% These outline the models to be used in compile function
+for i = 1:numel(R.chsim_name)
+    m.dipfit.model(i).source = R.chsim_name{i};
+end
+
+m.outstates = {[0 0 0 0 0 0 1 0]  [1 0]  [1 0]  [1 0]  [1 0]  [1 0]};
+R.obs.outstates = find([m.outstates{:}]);
+for i=1:numel(R.chloc_name)
+    R.obs.obsstates(i) = find(strcmp(R.chloc_name{i},R.chsim_name));
+end
+
+% Precompute xinds to make things easier with indexing
+% Compute X inds (removes need to spm_unvec which is slow)
+xinds = zeros(size(m.x,2),2);
+for i = 1:size(m.x,2)
+    if i == 1
+        xinds(i,1) = 1;
+        xinds(i,2) = size(m.x{i},2);
+    else
+        xinds(i,1) = xinds(i-1,2)+1;
+        xinds(i,2) = xinds(i,1) + (size(m.x{i},2)-1);
+    end
+end
+m.xinds = xinds;
+
+% setup exogenous noise
+% m.uset.p = DCM.Ep;
+m.uset.p.covar = eye(m.m);
+m.uset.p.scale = 5e1; %.*R.InstP.dt;
+uc = innovate_timeseries(R,m);
+
+%% Prepare Priors
+% 1 MMC
+% 2 STR
+% 3 GPE
+% 4 STN
+% 5 GPI
+% 6 THAL
+
+% Excitatory connections
+A = repmat(-32,m.m,m.m);
+A(2,1) = 0; % M1 to STR
+A(4,1) = 0; % M1 to STN
+A(3,4) = 0; % STN to GPe
+A(5,4) = 0; % STN to GPi
+A(1,6) = -1; % THAL to M1
+% 
+p.A{1} = A;
+A_s = repmat(1,size(A));
+p.A_s{1} = A_s;
+
+% Inhbitory connections
+A = repmat(-32,m.m,m.m);
+A(3,2) = 0; % STR to GPe
+A(5,2) = 0; % STR to GPi
+A(4,3) = 0; % GPe to STN
+A(5,3) = 0; % GPe to GPi
+% A(2,3) = 0; % GPe to STR
+A(6,5) = 0; % GPi to THAL
+p.A{2} = A;
+A_s = repmat(1,size(A));
+p.A_s{2} = A_s;
+
+% Connection strengths
+p.C = zeros(m.m,1);
+p.C_s = repmat(0.5,size(p.C));
+
+% Leadfield
+p.obs.LF = zeros(size(R.obs.LF)).*0.8;
+p.obs.LF_s = repmat(0.2,size(p.obs.LF));
+
+% Delays
+p.D = repmat(-32,size(p.A{1})).*~((p.A{1}>-32) | (p.A{2}>-32)) ;
+p.D_s = repmat(0.25,size(p.D));
+
+% Sigmoid transfer for connections
+p.S = [0 0];
+p.S_s = [0.5 0.5];
+
+% time constants and gains
+for i = 1:m.m
+    p.int{i}.T = zeros(1,m.Tint(i));
+    p.int{i}.T_s = repmat(1,size(p.int{i}.T));
+    p.int{i}.G = zeros(1,m.Gint(i));
+    p.int{i}.G_s = repmat(1,size(p.int{i}.G));
+    p.int{i}.S = zeros(1);
+    p.int{i}.S_s = repmat(0.5,size(p.int{i}.S));
+    
+    p.int{i}.BT = zeros(1,m.Tint(i));
+    p.int{i}.BT_s = repmat(1.5,size(p.int{i}.T));
+    
+end
---
+++ Projects/Rat_NPD/routine/rat_InDirect_ModelComp/Model_fitting/rat_NPD_InDirect_ModelFitMaster_FULLMOD.m
@@ -0,0 +1,66 @@
+% MASTER FUNCTION FOR MODEL FITTING
+% This function incorporates all of the nodes (including GPi/Thalamus)
+% Models are specified in ModelSpecs of project folder. Will batch fit
+% models to the data specified in fx prepareRatData. WML stores batch
+% progress and can be accessed by multiple MATLAB realizations.
+%%%%%%%%%%%%%%%%%%%%%%%%
+% IF FRESH START
+% delete([R.rootn 'outputs\' R.out.tag '\WorkingModList.mat'])
+
+% simAnnealAddPaths()
+clear ; close all
+
+% Close all msgboxes
+handles = allchild(0);
+tags = get(handles,'Tag');
+isMsg = strncmp(tags,'Msgbox_',7); % all message boxes have the tags in the format of Msgbox_*
+delete(handles(isMsg));
+
+% Add relevant paths for toolboxes
+% simAnnealAddPaths()
+rng(123213)
+
+%% Set Routine Pars
+R = simannealsetup_InDirect_ModelComp;
+% IF FRESH START
+% delete([R.rootn 'outputs\' R.out.tag '\WorkingModList.mat'])
+%% Prepare the data
+R = prepareRatData_InDirect_Group_NPD(R);
+
+try
+    load([R.rootn 'outputs\' R.out.tag '\WorkingModList'])
+    disp('Loaded Mod List!!')
+catch
+    WML = [];
+    mkdir([R.rootn 'outputs\' R.out.tag ]);
+    save([R.rootn 'outputs\' R.out.tag '\WorkingModList'],'WML')
+    disp('Making Mod List!!')
+end
+
+for modID = 9:10
+    if modID>9
+        LF = [0.8 0.5 0.5 0.5 0.5 0.5].*0.7; % Fit visually and for normalised data
+        R.chsim_name = {'MMC','STR','GPE','STN','GPI','THAL'};
+    end
+    load([R.rootn 'outputs\' R.out.tag '\WorkingModList'],'WML')
+    if ~any(intersect(WML,modID))
+        WML = [WML modID];
+        save([R.rootn 'outputs\' R.out.tag '\WorkingModList'],'WML')
+        disp('Writing to Mod List!!')
+        fprintf('Now Fitting Model %.0f',modID)
+        f = msgbox(sprintf('Fitting Model %.0f',modID));
+        
+        %% Prepare Model
+        modelspec = eval(['@MS_rat_' R.out.tag '_Model' num2str(modID)]);
+        [R p m uc] = modelspec(R);
+        pause(5)
+        R.out.dag = sprintf('NPD_InDrt_ModComp_M%.0f',modID); % 'All Cross'
+        
+        %% Run ABC Optimization
+        R = setSimTime(R,32);
+        R.Bcond = 0;
+        parBank = [];
+        R.SimAn.rep = 512; %448
+        [p] = SimAn_ABC_211218(m.x,uc,p,m,R,parBank);
+    end
+end
