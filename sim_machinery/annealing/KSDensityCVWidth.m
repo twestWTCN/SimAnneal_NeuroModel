80cfb240435dcf5ec71d9951c3244ad8bd3e8273
Tim West <twestWTCN@users.noreply.github.com>
2019-04-26 20:06:22 +0100

    Revert "Revert "KSDensityOptimization""
    This reverts commit 72246627940bc3ace3e7712a9fa3a9518801e913.

--- sim_machinery/annealing/KSDensityCVWidth.m
+++ sim_machinery/annealing/KSDensityCVWidth.m
@@ -1,18 +1,29 @@
-function u = KSDensityCVWidth(x,xf,W,range,n,fx)
+function u0 = KSDensityCVWidth(x,xf,W,range,n,fx)
 % Compute using default
-[~,~,u] = ksdensity(x,xf,'function',fx,'Weights',W);
+[f1,x1,u1] = ksdensity(x,xf,'function','pdf','Weights',W);
 % try other bandwidths
-uu = logspace(log10(u.*10^range(1)),log10(u.*10^range(2)),n);
+uu = logspace(log10(u1.*10^range(1)),log10(u1.*10^range(2)),n);
 v = zeros(size(uu));
 % using same partition each time reduces variation 
 cp = cvpartition(length(x),'kfold',10);
 for j=1:length(uu)
       % compute log likelihood for test data based on training data
-      loglik = @(xtr,xte) sum(log(ksdensity(xtr(:,1),xte(:,1),'function',fx,'Weights',xtr(:,3),'width',uu(j))));
+      loglik = @(xtr,xte) sum(log(ksdensity(xtr(:,1),xte(:,1),'function','pdf','Weights',xtr(:,3),'width',uu(j))));
       % sum across all train/test partitions
       v(j) = sum(crossval(loglik,[x' xf' W'],'partition',cp));
 end 
 
 [~,maxi] = max(v);
-u = uu(maxi);
-% [f0,x0,u] = ksdensity(x,'width',uu(maxi));
+u0 = uu(maxi);
+
+% % subplot(2,1,1)
+% % [f0,x0,u0] = ksdensity(x,xf,'function','pdf','Weights',W,'width',u0);
+% % scatter(x1,f1); hold on; scatter(x0,f0)
+% % histogram(x,'Normalization','pdf')
+% % subplot(2,1,2)
+% % 
+% % [fl0,xl0] = ksdensity(x,xf,'function',fx,'Weights',W,'width',u0);
+% % [fl1,xl1] = ksdensity(x,xf,'function',fx,'Weights',W,'width',u1);
+% % scatter(xl1,fl1); hold on; scatter(xl0,fl0)
+% % 
+% % close all
